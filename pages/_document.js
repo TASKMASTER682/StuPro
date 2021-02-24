5df5fec609f002ba521f4b5f2087f962800b41d7
TASKMASTER682 <sayedanwarulhaq@gmail.com>
2021-02-24 20:03:09 +0530

    seo change

--- pages/_document.js
+++ pages/_document.js
@@ -24,8 +24,8 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="content-type" content="text/html; charset=UTF-8"></meta>
        
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.1/css/all.css" 
          integrity="sha384-xxzQGERXS00kBmZW/6qxqJPyxW3UR0BPsL4c8ILaIWXva5kFi7TxkIIaMiKtqV1Q" crossOrigin="anonymous" ></link>
