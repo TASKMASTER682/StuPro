c8a84ace3d9d19f4e481a6c7a0308221b91354cb
TASKMASTER682 <sayedanwarulhaq@gmail.com>
2021-04-08 16:25:06 +0530

    new signal

--- pages/initOneSignal.js
+++ pages/initOneSignal.js
@@ -1,15 +1,14 @@
 import React, { useEffect } from "react";
-const OneSignal = async () => {
+const initOneSignal = async () => {
     const OneSignal = (await import('react-onesignal')).default
     OneSignal.initialize('9bbd3dff-ae1b-4c84-8a28-782f2b03ee5d')
+
+    useEffect(() => {
+        initOneSignal()
+      }, [])
+      return <></>
   }
-const initOneSignal=()=>{
-    
-  useEffect(() => {
-    OneSignal()
-  }, [])
-  return <></>
-}
+
 
 
   export default initOneSignal;
