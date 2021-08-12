27fe8ae59b8f090e11b02ed8b42bcf3a7c7a137d
TASKMASTER682 <sayedanwarulhaq@gmail.com>
2021-08-12 19:52:12 +0530

    small change

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports={
         API_PRODUCTION:'https://the-prograd.herokuapp.com/api',
         PRODUCTION:true,
         DOMAIN_DEVELOPMENT:'http://localhost:3000',
-        DOMAIN_PRODUCTION:'https://theprograd.com',
+        DOMAIN_PRODUCTION:'https://stu-pro-os7aenjln-sayedanwarulhaq-gmailcom.vercel.app/',
         FB_APP_ID:'721482821740858',
         DISQUS_SHORTNAME:'stupro-1',
         GOOGLE_CLIENT_ID:'589470153090-ktn1h4h0bc4jfcrd5uj47ibf8mfj6gp8.apps.googleusercontent.com',
