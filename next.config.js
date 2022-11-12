9b71aa5902454e8750cce66803c7699cbda35d04
TASKMASTER682 <sayedanwarulhaq@gmail.com>
2022-11-12 21:04:39 +0530

    new cyclic

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
     FB_APP_ID: '721482821740858',
     DISQUS_SHORTNAME: 'stupro-1',
     GOOGLE_CLIENT_ID: '589470153090-ktn1h4h0bc4jfcrd5uj47ibf8mfj6gp8.apps.googleusercontent.com',
-    API_PRODUCTION: 'https://the-prograd.herokuapp.com/api',
+    API_PRODUCTION: 'https://kind-lime-jackrabbit-slip.cyclic.app/api',
     DOMAIN_DEVELOPMENT: 'http://localhost:3000',
   },
 
